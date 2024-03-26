a82664521cb19e678deea6f1da76348b8adb2c16
Sanka Weerathunga <sdweerathunga5@gmail.com>
2024-03-25 21:59:22 -0400

    VPC module

--- main.tf
+++ main.tf
@@ -18,15 +18,47 @@ data "aws_vpc" "default"{
   default = true
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-   vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
+  
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.2"
+  name = "blog_new"
+
+  vpc_id = module.blog_vpc.vpc_id
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  engress_rules       = ["all-all"]
+  engress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog"{
